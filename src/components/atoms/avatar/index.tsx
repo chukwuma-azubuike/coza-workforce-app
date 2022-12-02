c23e7b8839d7e5ba1dc177f6cf086f04c22c01e6
Toluwalope Adeoye <toluade1512@gmail.com>
2022-12-02 20:14:44 +0100

    CGWA-31 tickets screen (#2)
    * CGWA-31 Tickets overview
    
    * CGWA-31 Tickets overview
    
    Co-authored-by: ToluAdeoye <toluade1512@gmail.com>

--- src/components/atoms/avatar/index.tsx
+++ src/components/atoms/avatar/index.tsx
@@ -25,3 +25,18 @@ const AvatarComponent: React.FC<IAvatarComponentProps> = props => {
 };
 
 export default AvatarComponent;
+
+export const AvatarComponentWithoutBadge: React.FC<IAvatarComponentProps> = ({
+    imageUrl,
+}) => {
+    return (
+        <Avatar
+            bg="amber.500"
+            source={{
+                uri: imageUrl,
+            }}
+            shadow={9}
+            size="sm"
+        />
+    );
+};
---
+++ src/views/app/tickets/tickets-by-date.tsx
@@ -0,0 +1,72 @@
+import { Center, HStack, Pressable, Text } from 'native-base';
+import React from 'react';
+import { AvatarComponentWithoutBadge } from '../../../components/atoms/avatar';
+import { Line } from '../../../components/atoms/line';
+
+type Props = {
+    data: {
+        date: string;
+        tickets: {
+            offender: string;
+            image: string;
+            ticket_description: string;
+            offense: string;
+            ticket_type: string;
+            department: string;
+            date: string;
+        }[];
+    };
+    handlePress: (component: string, data: {}) => void;
+};
+
+const TicketsByDate: React.FC<Props> = ({ data, handlePress }) => {
+    return (
+        <>
+            <Text pl="3" fontWeight="semibold">
+                {data?.date}
+            </Text>
+            <Line
+                color={'#888787'}
+                width="full"
+                height="1px"
+                marginTop="5px"
+                marginBottom="10px"
+            />
+
+            {data?.tickets?.map((ticket, index) => (
+                <Pressable
+                    key={`ticket-item-${index}`}
+                    onPress={() => handlePress('Ticket Details', ticket)}
+                    marginBottom="10px"
+                >
+                    <HStack
+                        pl={3}
+                        pr={6}
+                        space={3}
+                        justifyContent="space-between"
+                    >
+                        <HStack space={3} alignItems="center">
+                            <AvatarComponentWithoutBadge
+                                imageUrl={ticket?.image}
+                            />
+                            <Text>{ticket?.ticket_description}</Text>
+                        </HStack>
+                        <Center
+                            p={'5px 20px'}
+                            bgColor={'#E10101'}
+                            borderRadius="7px"
+                            borderColor={'#767575'}
+                            borderWidth="0.5"
+                        >
+                            <Text color={'#fff'} fontWeight="bold">
+                                {ticket?.offense}
+                            </Text>
+                        </Center>
+                    </HStack>
+                </Pressable>
+            ))}
+        </>
+    );
+};
+
+export default TicketsByDate;
