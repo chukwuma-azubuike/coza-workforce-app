b12cefca693d86f7a93d9d80a748ae69fb8d5cce
Toluwalope Adeoye <toluade1512@gmail.com>
2022-12-02 21:31:34 +0100

    CGWA-31 tickets screen (#3)
    * CGWA-31 Tickets overview
    
    * CGWA-31 Tickets overview
    
    * CGWA-31 Tickets overview
    
    Co-authored-by: ToluAdeoye <toluwaloope.adeoye@habaripay.com>

--- src/views/app/tickets/ticket-details.tsx
+++ src/views/app/tickets/ticket-details.tsx
@@ -3,16 +3,17 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import {
     Button,
     HStack,
-    KeyboardAvoidingView,
+    // KeyboardAvoidingView,
     Text,
     TextArea,
     VStack,
 } from 'native-base';
 import React from 'react';
 import { Line } from '../../../components/atoms/line';
-// import ModalAlertComponent from '../../../components/composite/modal-alert';
+import ModalAlertComponent from '../../../components/composite/modal-alert';
 import ViewWrapper from '../../../components/layout/viewWrapper';
-// import useModal from '../../../hooks/modal/useModal';
+import { THEME_CONFIG } from '../../../config/appConfig';
+import useModal from '../../../hooks/modal/useModal';
 
 type ItemProp = {
     title: string;
@@ -49,44 +50,54 @@ const TicketDetails: React.FC<NativeStackScreenProps<ParamListBase>> = ({
 
     const [dispute, setDispute] = React.useState<boolean>(false);
     const [disputeComment, setDisputeComment] = React.useState<string>('');
-    // const [isLoading, setIsLoading] = React.useState<boolean>(false);
+    const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
-    // const { setModalState } = useModal();
+    const { setModalState } = useModal();
 
-    // const handleDisputeSubmit = () => {
-    //     if (!isLoading) {
-    //         setIsLoading(prev => !prev);
-    //         setTimeout(() => {
-    //             setIsLoading(false);
-    //             setModalState({
-    //                 duration: 6,
-    //                 render: (
-    //                     <ModalAlertComponent
-    //                         description={
-    //                             disputeComment !== ""
-    //                                 ? `You clocked in at ${moment().format(
-    //                                       'LT'
-    //                                   )}`
-    //                                 : 'Please'
-    //                         }
-    //                         iconType={
-    //                             isInRange ? 'material-community' : 'ionicon'
-    //                         }
-    //                         color={
-    //                             isInRange
-    //                                 ? THEME_CONFIG.primaryLight
-    //                                 : undefined
-    //                         }
-    //                         iconName={
-    //                             isInRange ? 'timer-outline' : 'warning-outline'
-    //                         }
-    //                         status={isInRange ? 'success' : 'warning'}
-    //                     />
-    //                 ),
-    //             });
-    //         }, 2000);
-    //     }
-    // };
+    const handleDisputeSubmit = () => {
+        if (!isLoading) {
+            setIsLoading(prev => !prev);
+            setTimeout(() => {
+                setIsLoading(false);
+                setModalState({
+                    duration: 6,
+                    render: (
+                        <ModalAlertComponent
+                            description={
+                                disputeComment !== ''
+                                    ? 'Your dispute on this ticket has been submitted.'
+                                    : 'Please share your concerns on this ticket'
+                            }
+                            iconType={
+                                disputeComment !== ''
+                                    ? 'material-community'
+                                    : 'ionicon'
+                            }
+                            color={
+                                disputeComment !== ''
+                                    ? THEME_CONFIG.success
+                                    : THEME_CONFIG.warning
+                            }
+                            iconName={
+                                disputeComment !== ''
+                                    ? 'timer-outline'
+                                    : 'warning-outline'
+                            }
+                            status={
+                                disputeComment !== '' ? 'success' : 'warning'
+                            }
+                            backgroundColor={'#fff'}
+                        />
+                    ),
+                });
+            }, 200);
+        }
+    };
+
+    const cancel = () => {
+        setDispute(false);
+        setDisputeComment('');
+    };
 
     React.useEffect(() => {
         setDispute(false);
@@ -97,136 +108,132 @@ const TicketDetails: React.FC<NativeStackScreenProps<ParamListBase>> = ({
     }, []);
 
     return (
-        <KeyboardAvoidingView>
-            <ViewWrapper>
-                <VStack
-                    space={2}
-                    margin={'20px'}
-                    padding={'40px 40px'}
-                    bgColor={'#F3F2F2'}
-                >
-                    <TicketItem title="Date" value={data?.date} />
-                    <Line
-                        color={'#000'}
-                        width="full"
-                        height="0.3px"
-                        marginTop="5px"
-                        marginBottom="10px"
-                    />
-                    <TicketItem title="Department" value={data?.department} />
-                    <Line
-                        color={'#000'}
-                        width="full"
-                        height="0.3px"
-                        marginTop="5px"
-                        marginBottom="10px"
-                    />
-                    <TicketItem title="Ticket Type" value={data?.ticket_type} />
-                    <Line
-                        color={'#000'}
-                        width="full"
-                        height="0.3px"
-                        marginTop="5px"
-                        marginBottom="10px"
-                    />
+        <ViewWrapper>
+            <VStack
+                space={2}
+                margin={'20px'}
+                padding={'40px 40px'}
+                bgColor={'#F3F2F2'}
+            >
+                <TicketItem title="Date" value={data?.date} />
+                <Line
+                    color={'#000'}
+                    width="full"
+                    height="0.3px"
+                    marginTop="5px"
+                    marginBottom="10px"
+                />
+                <TicketItem title="Department" value={data?.department} />
+                <Line
+                    color={'#000'}
+                    width="full"
+                    height="0.3px"
+                    marginTop="5px"
+                    marginBottom="10px"
+                />
+                <TicketItem title="Ticket Type" value={data?.ticket_type} />
+                <Line
+                    color={'#000'}
+                    width="full"
+                    height="0.3px"
+                    marginTop="5px"
+                    marginBottom="10px"
+                />
 
-                    <TicketItem title="Offender" value={data?.offender} />
-                    <Line
-                        color={'#000'}
-                        width="full"
-                        height="0.3px"
-                        marginTop="5px"
-                        marginBottom="10px"
-                    />
-                    <TicketItem title="Offense" value={data?.offense} />
-                    <Line
-                        color={'#000'}
-                        width="full"
-                        height="0.3px"
-                        marginTop="5px"
-                        marginBottom="10px"
-                    />
-                    <TicketItem
-                        title="Details"
-                        value={data?.ticket_description}
-                    />
+                <TicketItem title="Offender" value={data?.offender} />
+                <Line
+                    color={'#000'}
+                    width="full"
+                    height="0.3px"
+                    marginTop="5px"
+                    marginBottom="10px"
+                />
+                <TicketItem title="Offense" value={data?.offense} />
+                <Line
+                    color={'#000'}
+                    width="full"
+                    height="0.3px"
+                    marginTop="5px"
+                    marginBottom="10px"
+                />
+                <TicketItem title="Details" value={data?.ticket_description} />
 
-                    {!dispute ? (
+                {!dispute ? (
+                    <HStack
+                        key={'ticket-buttons'}
+                        space={4}
+                        marginTop={'40px'}
+                        justifyContent="center"
+                    >
+                        <Button
+                            onPress={() => setDispute(true)}
+                            variant="outline"
+                            size="lg"
+                            borderColor={'#6B079C'}
+                            borderRadius="7px"
+                            _text={{ fontSize: '17px', fontWeight: '900' }}
+                        >
+                            Dispute
+                        </Button>
+                        <Button
+                            size="lg"
+                            borderRadius="7px"
+                            _text={{ fontSize: '17px', fontWeight: '900' }}
+                        >
+                            Acknowledge
+                        </Button>
+                    </HStack>
+                ) : (
+                    <>
+                        <TextArea
+                            value={disputeComment}
+                            onChangeText={text => setDisputeComment(text)}
+                            bgColor={'#fff'}
+                            h={'179px'}
+                            marginTop={'20px'}
+                            placeholder="Share concerns on this ticket."
+                            placeholderTextColor={'#D0D0D0'}
+                            w="100%"
+                            borderColor={'#9F9F9F'}
+                            borderWidth={'0.3px'}
+                        />
+
                         <HStack
-                            key={'ticket-buttons'}
+                            key={'dispute-box-button'}
                             space={4}
-                            marginTop={'40px'}
+                            marginTop={'20px'}
                             justifyContent="center"
                         >
                             <Button
-                                onPress={() => setDispute(true)}
+                                key={'canel-dispute'}
+                                onPress={cancel}
                                 variant="outline"
                                 size="lg"
                                 borderColor={'#6B079C'}
                                 borderRadius="7px"
-                                _text={{ fontSize: '17px', fontWeight: '900' }}
+                                _text={{
+                                    fontSize: '17px',
+                                    fontWeight: '900',
+                                }}
                             >
-                                Dispute
+                                Cancel
                             </Button>
                             <Button
+                                onPress={handleDisputeSubmit}
                                 size="lg"
                                 borderRadius="7px"
-                                _text={{ fontSize: '17px', fontWeight: '900' }}
+                                _text={{
+                                    fontSize: '17px',
+                                    fontWeight: '900',
+                                }}
                             >
-                                Acknowledge
+                                Submit
                             </Button>
                         </HStack>
-                    ) : (
-                        <>
-                            <TextArea
-                                value={disputeComment}
-                                onChangeText={text => setDisputeComment(text)}
-                                bgColor={'#fff'}
-                                h={'179px'}
-                                marginTop={'20px'}
-                                placeholder="Share concerns on this ticket."
-                                placeholderTextColor={'#D0D0D0'}
-                                w="100%"
-                                borderColor={'#9F9F9F'}
-                                borderWidth={'0.3px'}
-                            />
-
-                            <HStack
-                                key={'dispute-box-button'}
-                                space={4}
-                                marginTop={'20px'}
-                                justifyContent="center"
-                            >
-                                <Button
-                                    key={'canel-dispute'}
-                                    onPress={() => setDispute(false)}
-                                    variant="outline"
-                                    size="lg"
-                                    borderColor={'#6B079C'}
-                                    borderRadius="7px"
-                                    _text={{
-                                        fontSize: '17px',
-                                        fontWeight: '900',
-                                    }}
-                                >
-                                    Cancel
-                                </Button>
-                                <Button
-                                    size="lg"
-                                    borderRadius="7px"
-                                    _text={{
-                                        fontSize: '17px',
-                                        fontWeight: '900',
-                                    }}
-                                >
-                                    Submit
-                                </Button>
-                            </HStack>
-                        </>
-                    )}
-                </VStack>
-            </ViewWrapper>
-        </KeyboardAvoidingView>
+                    </>
+                )}
+            </VStack>
+        </ViewWrapper>
     );
 };
 
