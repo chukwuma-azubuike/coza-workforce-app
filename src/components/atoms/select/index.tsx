b5088fd64ba4bfd74e7aeae08af813b09619e5db
Chukwuma Azubuike <chukwuma.azubuike@ajocard.com>
2023-02-04 23:47:51 +0100

    New Select Picker component

--- src/components/atoms/select/index.tsx
+++ src/components/atoms/select/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { ISelectItemProps, ISelectProps, Select } from 'native-base';
+import { Picker, PickerIOS } from '@react-native-picker/picker';
+import { ISelectItemProps, ISelectProps } from 'native-base';
 import { THEME_CONFIG } from '../../../config/appConfig';
 import { Icon } from '@rneui/themed';
 import { IIconTypes } from '../../../utils/types';
 import useAppColorMode from '../../../hooks/theme/colorMode';
+import { Platform } from 'react-native';
 
 interface ISelectComponent extends ISelectProps {}
 interface ISelectItemComponent extends ISelectItemProps {
@@ -11,22 +13,36 @@ interface ISelectItemComponent extends ISelectItemProps {
 }
 
 const SelectComponent = (props: ISelectComponent) => {
+    const { isLightMode } = useAppColorMode();
+
+    const PickerWrapper = Platform.OS === 'ios' ? PickerIOS : (Picker as any);
+
     return (
-        <Select
-            p={3}
-            w="100%"
-            size="lg"
-            _light={{
-                bg: 'gray.100',
+        <PickerWrapper
+            style={{
+                backgroundColor: isLightMode
+                    ? THEME_CONFIG.veryLightGray
+                    : THEME_CONFIG.darkGray,
+                color: isLightMode
+                    ? THEME_CONFIG.darkGray
+                    : THEME_CONFIG.veryLightGray,
+                borderRadius: 100,
+                borderWidth: 2,
+                borderColor: isLightMode
+                    ? THEME_CONFIG.lightGray
+                    : THEME_CONFIG.darkGray,
             }}
-            _dark={{
-                bg: 'gray.900',
+            // {...props}
+            itemStyle={{
+                backgroundColor: 'red',
+                color: isLightMode
+                    ? THEME_CONFIG.darkGray
+                    : THEME_CONFIG.veryLightGray,
+                borderRadius: 100,
             }}
-            {...props}
-            borderRadius={THEME_CONFIG.borderRadius}
         >
             {props.children}
-        </Select>
+        </PickerWrapper>
     );
 };
 
@@ -34,24 +50,31 @@ const SelectItemComponent = (props: ISelectItemComponent) => {
     const { icon } = props;
     const { isLightMode } = useAppColorMode();
 
+    const PickerItem = Platform.OS === 'ios' ? PickerIOS.Item : Picker.Item;
+
     return (
-        <Select.Item
-            {...props}
-            leftIcon={
-                icon ? (
-                    <Icon
-                        size={22}
-                        name={icon.name}
-                        type={icon.type}
-                        style={{ marginLeft: 14 }}
-                        color={
-                            isLightMode
-                                ? THEME_CONFIG.gray
-                                : THEME_CONFIG.veryLightGray
-                        }
-                    />
-                ) : undefined
+        <PickerItem
+            label={props.label}
+            value={props.value}
+            color={
+                isLightMode ? THEME_CONFIG.darkGray : THEME_CONFIG.veryLightGray
             }
+            // {...props}
+            // leftIcon={
+            //     icon ? (
+            //         <Icon
+            //             size={22}
+            //             name={icon.name}
+            //             type={icon.type}
+            //             style={{ marginLeft: 14 }}
+            //             color={
+            //                 isLightMode
+            //                     ? THEME_CONFIG.gray
+            //                     : THEME_CONFIG.veryLightGray
+            //             }
+            //         />
+            //     ) : undefined
+            // }
         />
     );
 };
