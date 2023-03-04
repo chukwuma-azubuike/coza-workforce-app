fb3a98cced5b67a6864384bb6b841635fa71f526
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2023-03-04 18:16:05 +0100

    tidy campus attendance table

--- src/views/app/attendance/flatListConfig.tsx
+++ src/views/app/attendance/flatListConfig.tsx
@@ -7,6 +7,8 @@ import { IFlatListColumn } from '../../../components/composite/flat-list';
 import { IAttendance } from '../../../store/types';
 import Utils from '../../../utils';
 import { Appearance } from 'react-native';
+import { AvatarComponentWithoutBadge } from '../../../components/atoms/avatar';
+import { AVATAR_FALLBACK_URL } from '../../../constants';
 
 const colorScheme = Appearance.getColorScheme();
 
@@ -226,4 +228,68 @@ const campusColumns: IFlatListColumn[] = [
     },
 ];
 
-export { campusColumns, myAttendanceColumns, teamAttendanceDataColumns };
+const campusColumns_1: IFlatListColumn[] = [
+    {
+        title: 'User',
+        dataIndex: 'user',
+        render: (elm: IAttendance, key) => (
+            <HStack key={`user-${key}`} alignItems="center" space={4} flex={1} textAlign="left" w="full">
+                <AvatarComponentWithoutBadge
+                    badge
+                    mr={4}
+                    size="xs"
+                    imageUrl={elm.user?.pictureUrl || AVATAR_FALLBACK_URL}
+                />
+                <VStack>
+                    <HStack>
+                        <Text bold color={isLightMode ? 'gray.800' : 'gray.200'} ml={2}>
+                            {Utils.truncateString(elm.user?.firstName)}
+                        </Text>
+                        <Text bold color={isLightMode ? 'gray.800' : 'gray.200'} ml={2}>
+                            {Utils.truncateString(elm.user?.lastName)}
+                        </Text>
+                    </HStack>
+                    <Text color={isLightMode ? 'gray.800' : 'gray.200'} ml={2}>
+                        {Utils.truncateString(elm.user?.department.departmentName)}
+                    </Text>
+                </VStack>
+            </HStack>
+        ),
+    },
+    {
+        title: '',
+        dataIndex: 'clockin',
+        render: (elm: IAttendance, key) => (
+            <HStack key={`clockin-${key}`} alignItems="center">
+                <Icon color={THEME_CONFIG.primaryLight} name="arrow-down-right" type="feather" size={18} />
+                <Text
+                    _dark={{
+                        color: elm.clockIn ? 'green.300' : 'red.300',
+                    }}
+                    color={elm.clockIn ? 'green.500' : 'red.500'}
+                >
+                    {elm.clockIn ? moment(elm.clockIn).format('LT') : '--:--'}
+                </Text>
+            </HStack>
+        ),
+    },
+    {
+        title: 'Clock In         Clock Out',
+        dataIndex: 'clockout',
+        render: (elm: IAttendance, key) => (
+            <HStack key={`clockout-${key}`} alignItems="center">
+                <Icon color={THEME_CONFIG.primaryLight} name="arrow-up-right" type="feather" size={18} />
+                <Text
+                    color="gray.500"
+                    _dark={{
+                        color: 'warmGray.200',
+                    }}
+                >
+                    {elm.clockOut ? moment(elm.clockOut).format('LT') : '--:--'}
+                </Text>
+            </HStack>
+        ),
+    },
+];
+
+export { campusColumns, campusColumns_1, myAttendanceColumns, teamAttendanceDataColumns };
