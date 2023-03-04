b592d2a1f26e7a42f55355c0420a8e85f3057cc8
ToluAdeoye <toluwaloope.adeoye@habaripay.com>
2023-03-04 18:08:51 +0100

    Included support link in forgot password and reset pasword pages

--- src/views/auth/reset-password/index.tsx
+++ src/views/auth/reset-password/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Center, FormControl, Heading, HStack, Stack, Text, VStack } from 'native-base';
+import { Box, FormControl, Heading, HStack, Stack, Text, VStack } from 'native-base';
 import { InputComponent } from '../../../components/atoms/input';
 import ButtonComponent from '../../../components/atoms/button';
 import ViewWrapper from '../../../components/layout/viewWrapper';
@@ -13,6 +13,7 @@ import { RegisterSchema } from '../../../utils/schemas';
 import { TouchableRipple } from 'react-native-paper';
 import Logo from '../../../components/atoms/logo';
 import { ParamListBase } from '@react-navigation/native';
+import SupportLink from '../support-link';
 
 const ResetPassword: React.FC<IRegistrationPageStep<NativeStackScreenProps<ParamListBase>>> = ({
     errors,
@@ -38,129 +39,132 @@ const ResetPassword: React.FC<IRegistrationPageStep<NativeStackScreenProps<Param
 
     return (
         <ViewWrapper>
-            {/* <Center flex={1}> */}
-            <VStack space={6} pb={5} px={4} pt={10} alignItems="center" justifyContent="space-around">
-                <Logo />
-                <Heading textAlign="left">Reset password</Heading>
-                <Box alignItems="center" w="100%">
-                    <Formik
-                        validateOnChange
-                        enableReinitialize
-                        onSubmit={handleSubmit}
-                        initialValues={init}
-                        validationSchema={RegisterSchema}
-                    >
-                        {({
-                            errors,
-                            handleChange,
-                            handleSubmit,
-                        }: Pick<
-                            IRegisterFormProps,
-                            'values' | 'errors' | 'handleChange' | 'handleSubmit' | 'setFieldError'
-                        >) => {
-                            return (
-                                <Stack w="100%" space={1}>
-                                    <FormControl isRequired isInvalid={errors?.password ? true : false}>
-                                        <FormControl.Label>Password</FormControl.Label>
-                                        <InputComponent
-                                            type={showPassword ? 'text' : 'password'}
-                                            placeholder="password"
-                                            leftIcon={{
-                                                name: 'lock-closed-outline',
-                                                type: 'ionicon',
-                                            }}
-                                            rightIcon={{
-                                                name: showPassword ? 'eye-off-outline' : 'eye-outline',
-                                                type: 'ionicon',
-                                            }}
-                                            onIconPress={handleIconPress}
-                                            onChangeText={handleChange('password')}
-                                        />
-                                        <FormControl.ErrorMessage
-                                            fontSize="2xl"
-                                            mt={3}
-                                            leftIcon={
-                                                <Icon
-                                                    size={16}
-                                                    name="warning"
-                                                    type="antdesign"
-                                                    color={THEME_CONFIG.error}
-                                                />
-                                            }
-                                        >
-                                            {errors?.password}
-                                        </FormControl.ErrorMessage>
-                                    </FormControl>
-                                    <FormControl isRequired isInvalid={errors?.confirmPassword ? true : false}>
-                                        <FormControl.Label>Confirm password</FormControl.Label>
-                                        <InputComponent
-                                            type={showPassword ? 'text' : 'password'}
-                                            placeholder="Repeat password"
-                                            leftIcon={{
-                                                name: 'lock-closed-outline',
-                                                type: 'ionicon',
-                                            }}
-                                            rightIcon={{
-                                                name: showPassword ? 'eye-off-outline' : 'eye-outline',
-                                                type: 'ionicon',
-                                            }}
-                                            onIconPress={handleIconPress}
-                                            onChangeText={handleChange('confirmPassword')}
-                                        />
-                                        <FormControl.ErrorMessage
-                                            fontSize="2xl"
-                                            mt={3}
-                                            leftIcon={
-                                                <Icon
-                                                    size={16}
-                                                    name="warning"
-                                                    type="antdesign"
-                                                    color={THEME_CONFIG.error}
-                                                />
-                                            }
-                                        >
-                                            {errors?.confirmPassword}
-                                        </FormControl.ErrorMessage>
-                                    </FormControl>
-                                    <FormControl>
-                                        <HStack space={4} justifyContent="center">
-                                            {/* <ButtonComponent onPress={handleBackPress} width={160} secondary mt={4}>
+            <Box w="100%" h="full" justifyContent="space-between" pb={4}>
+                <VStack space={6} pb={5} px={4} pt={10} alignItems="center" justifyContent="space-around">
+                    <Logo />
+                    <Heading textAlign="left">Reset password</Heading>
+                    <Box alignItems="center" w="100%">
+                        <Formik
+                            validateOnChange
+                            enableReinitialize
+                            onSubmit={handleSubmit}
+                            initialValues={init}
+                            validationSchema={RegisterSchema}
+                        >
+                            {({
+                                errors,
+                                handleChange,
+                                handleSubmit,
+                            }: Pick<
+                                IRegisterFormProps,
+                                'values' | 'errors' | 'handleChange' | 'handleSubmit' | 'setFieldError'
+                            >) => {
+                                return (
+                                    <Stack w="100%" space={1}>
+                                        <FormControl isRequired isInvalid={errors?.password ? true : false}>
+                                            <FormControl.Label>Password</FormControl.Label>
+                                            <InputComponent
+                                                type={showPassword ? 'text' : 'password'}
+                                                placeholder="password"
+                                                leftIcon={{
+                                                    name: 'lock-closed-outline',
+                                                    type: 'ionicon',
+                                                }}
+                                                rightIcon={{
+                                                    name: showPassword ? 'eye-off-outline' : 'eye-outline',
+                                                    type: 'ionicon',
+                                                }}
+                                                onIconPress={handleIconPress}
+                                                onChangeText={handleChange('password')}
+                                            />
+                                            <FormControl.ErrorMessage
+                                                fontSize="2xl"
+                                                mt={3}
+                                                leftIcon={
+                                                    <Icon
+                                                        size={16}
+                                                        name="warning"
+                                                        type="antdesign"
+                                                        color={THEME_CONFIG.error}
+                                                    />
+                                                }
+                                            >
+                                                {errors?.password}
+                                            </FormControl.ErrorMessage>
+                                        </FormControl>
+                                        <FormControl isRequired isInvalid={errors?.confirmPassword ? true : false}>
+                                            <FormControl.Label>Confirm password</FormControl.Label>
+                                            <InputComponent
+                                                type={showPassword ? 'text' : 'password'}
+                                                placeholder="Repeat password"
+                                                leftIcon={{
+                                                    name: 'lock-closed-outline',
+                                                    type: 'ionicon',
+                                                }}
+                                                rightIcon={{
+                                                    name: showPassword ? 'eye-off-outline' : 'eye-outline',
+                                                    type: 'ionicon',
+                                                }}
+                                                onIconPress={handleIconPress}
+                                                onChangeText={handleChange('confirmPassword')}
+                                            />
+                                            <FormControl.ErrorMessage
+                                                fontSize="2xl"
+                                                mt={3}
+                                                leftIcon={
+                                                    <Icon
+                                                        size={16}
+                                                        name="warning"
+                                                        type="antdesign"
+                                                        color={THEME_CONFIG.error}
+                                                    />
+                                                }
+                                            >
+                                                {errors?.confirmPassword}
+                                            </FormControl.ErrorMessage>
+                                        </FormControl>
+                                        <FormControl>
+                                            <HStack space={4} justifyContent="center">
+                                                {/* <ButtonComponent onPress={handleBackPress} width={160} secondary mt={4}>
                                         Go back
                                     </ButtonComponent> */}
-                                            <ButtonComponent
-                                                isLoading={isLoading || loginIsLoading}
-                                                isLoadingText={loginIsLoading ? 'Logging in...' : 'Signing up...'}
-                                                // onPress={onSubmit}
-                                                onPress={() => navigation.navigate('Login')}
-                                                width={160}
-                                                mt={4}
-                                            >
-                                                Save
-                                            </ButtonComponent>
-                                        </HStack>
-                                    </FormControl>
-                                </Stack>
-                            );
-                        }}
-                    </Formik>
-                </Box>
+                                                <ButtonComponent
+                                                    isLoading={isLoading || loginIsLoading}
+                                                    isLoadingText={loginIsLoading ? 'Logging in...' : 'Signing up...'}
+                                                    // onPress={onSubmit}
+                                                    onPress={() => navigation.navigate('Login')}
+                                                    width={160}
+                                                    mt={4}
+                                                >
+                                                    Save
+                                                </ButtonComponent>
+                                            </HStack>
+                                        </FormControl>
+                                    </Stack>
+                                );
+                            }}
+                        </Formik>
+                    </Box>
 
-                <HStack alignItems="center" justifyContent="center">
-                    <Text fontSize="md" color="gray.400">
-                        Remember your password?
-                    </Text>
-                    <TouchableRipple
-                        style={{ paddingHorizontal: 6, borderRadius: 10 }}
-                        rippleColor="rgba(255, 255, 255, 0)"
-                        onPress={() => navigation.navigate('Login')}
-                    >
-                        <Text fontSize="md" _dark={{ color: 'primary.400' }} _light={{ color: 'primary.500' }}>
-                            Login
+                    <HStack alignItems="center" justifyContent="center">
+                        <Text fontSize="md" color="gray.400">
+                            Remember your password?
                         </Text>
-                    </TouchableRipple>
-                </HStack>
-            </VStack>
-            {/* </Center> */}
+                        <TouchableRipple
+                            style={{ paddingHorizontal: 6, borderRadius: 10 }}
+                            rippleColor="rgba(255, 255, 255, 0)"
+                            onPress={() => navigation.navigate('Login')}
+                        >
+                            <Text fontSize="md" _dark={{ color: 'primary.400' }} _light={{ color: 'primary.500' }}>
+                                Login
+                            </Text>
+                        </TouchableRipple>
+                    </HStack>
+                </VStack>
+                <Box w="full" justifyContent="center" justifyItems="center" alignItems="center">
+                    <SupportLink />
+                </Box>
+            </Box>
         </ViewWrapper>
     );
 };
