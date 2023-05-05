725260169da47b388fd9f5b0c966b349e5dbe9c2
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2023-05-05 22:06:04 +0100

    develop into IOS-Setup

---
+++ src/store/services/role.ts
@@ -0,0 +1,30 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+import { IRole, IDefaultResponse } from '../types';
+import { fetchUtils } from './fetch-utils';
+
+const SERVICE_URL = 'role';
+
+export const roleServiceSlice = createApi({
+    reducerPath: SERVICE_URL,
+
+    baseQuery: fetchUtils.baseQuery,
+
+    endpoints: endpoint => ({
+        getRoles: endpoint.query<IRole[], void>({
+            query: () => `/${SERVICE_URL}/getRoles`,
+
+            transformResponse: (response: IDefaultResponse<IRole[]>) => response.data,
+        }),
+
+        getRoleById: endpoint.query<IRole, IRole['_id']>({
+            query: id => `/${SERVICE_URL}/getRoles/${id}`,
+
+            transformResponse: (response: IDefaultResponse<IRole>) => response.data,
+        }),
+
+        // Add your endpoints here
+    }),
+});
+
+// Use exported hook in relevant components
+export const { useGetRolesQuery, useGetRoleByIdQuery } = roleServiceSlice;
