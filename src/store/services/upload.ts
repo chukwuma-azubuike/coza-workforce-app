6ebb5545530816f3d1c947c1c98aa435da6d2e64
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2024-10-02 13:44:54 +0100

    s3 upload process flow

--- src/store/services/upload.ts
+++ src/store/services/upload.ts
@@ -1,31 +1,74 @@
-import { UPLOAD_URL } from '@env';
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { IStore } from '..';
-import { IDefaultResponse, REST_API_VERBS } from '../types';
+import { Dispatch, SetStateAction } from 'react';
 
-const SERVICE_URL = 'upload';
+import { createApi } from '@reduxjs/toolkit/query/react';
+import { Dispatch as ReduxDispatch } from '@reduxjs/toolkit';
+
+import { axiosBaseQueryFn } from './fetch-utils';
+import { uploadActions } from '@store/actions/upload';
+import { IDefaultResponse, REST_API_VERBS } from '@store/types';
+import { Asset } from 'react-native-image-picker';
+
+type IUploadResponse = IDefaultResponse<{}>; // TODO: Update once upload endpoint is available
+type IGenerateUploadURLResponse = IDefaultResponse<{ url: string }>;
+interface IUploadPayload {
+    id?: string;
+    file: Asset;
+    url: string;
+    dispatch?: ReduxDispatch; // Passed as a return from useDispatch hook in React component
+    setProgress?: Dispatch<SetStateAction<number>>;
+    description?: string;
+}
+
+const SERVICE_URL = 'uploadApi';
+
+const PROXY_URL = process.env.NEXT_PUBLIC_PROXY_URL;
+const UPLOAD_URL = `${PROXY_URL}${process.env.NEXT_PUBLIC_IMGBB_UPLOAD_URL}`;
 
 export const uploadServiceSlice = createApi({
     reducerPath: SERVICE_URL,
 
-    baseQuery: fetchBaseQuery({
-        baseUrl: UPLOAD_URL,
+    tagTypes: [SERVICE_URL],
 
-        prepareHeaders: async (headers, { getState }) => {
-            const token = (getState() as IStore).account;
+    baseQuery: axiosBaseQueryFn({ baseUrl: UPLOAD_URL }),
 
-            if (token) headers.set('authorization', `Bearer ${token}`);
+    endpoints: builder => ({
+        generateUploadUrl: builder.mutation<string, { file_key: string }>({
+            query: ({ ...data }) => ({
+                url: 'presigned_url_endpoint',
+                method: REST_API_VERBS.POST,
+                data,
+            }),
 
-            return headers;
-        },
+            transformResponse: (response: IGenerateUploadURLResponse) => response?.data.url,
+        }),
 
-        // timeout: 60000,
-    }),
+        upload: builder.mutation<IUploadResponse['data'], IUploadPayload>({
+            query: ({ id, file, url, dispatch, setProgress, description }) => ({
+                url,
+                data: file,
+                method: REST_API_VERBS.PUT,
+                headers: { 'Content-Type': file.type },
 
-    endpoints: endpoint => ({
-        // Add your endpoints here
+                // Capture upload progress
+                onUploadProgress({ loaded, total }) {
+                    if (total) {
+                        const progress = Math.round((loaded * 100) / total);
+
+                        if (setProgress) {
+                            setProgress(progress);
+                        }
+
+                        if (dispatch && id) {
+                            dispatch(uploadActions.updateProgress({ id, total, loaded, description }));
+                        }
+                    }
+                },
+            }),
+
+            transformResponse: (response: IUploadResponse) => response?.data,
+        }),
     }),
 });
 
 // Use exported hook in relevant components
-export const {} = uploadServiceSlice;
+export const { useGenerateUploadUrlMutation, useUploadMutation } = uploadServiceSlice;
