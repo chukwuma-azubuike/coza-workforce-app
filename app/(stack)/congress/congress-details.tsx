3298aa7bb250a24b9cc3b962103495da693d1a4f
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-04-26 19:10:11 +0100

    change CGLS & CGWC to COngress nomenclature

---
+++ app/(stack)/congress/congress-details.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CongressDetailsScreen: React.FC = () => {
+    return <></>;
+};
+
+export default CongressDetailsScreen;
---
+++ views/app/congress/create-instant-message.tsx
@@ -0,0 +1,130 @@
+import { View } from "react-native";
+import { ParamListBase } from '@react-navigation/native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Icon } from '@rneui/themed';
+import { Formik, FormikConfig } from 'formik';
+import { FormControl } from 'native-base';
+import React from 'react';
+import ButtonComponent from '@components/atoms/button';
+import ViewWrapper from '@components/layout/viewWrapper';
+import { THEME_CONFIG } from '@config/appConfig';
+import useModal from '@hooks/modal/useModal';
+import { ICongressInstantMessage } from '@store/types';
+import { CreateCongressInstantMessageSchema } from '@utils/schemas';
+import { useCreateCongressInstantMessagesMutation } from '@store/services/congress';
+import TextAreaComponent from '@components/atoms/text-area';
+
+const CreateCongressInstantMessage: React.FC<NativeStackScreenProps<ParamListBase>> = ({ navigation, route }) => {
+    const { navigate } = navigation;
+    const params = route.params as { CongressId: string };
+    const CongressId = params?.CongressId;
+    const { setModalState } = useModal();
+
+    const [createCongressInstantMessage, { isLoading, error, data, reset }] = useCreateCongressInstantMessagesMutation();
+
+    const onSubmit: FormikConfig<ICongressInstantMessage>['onSubmit'] = async (values, { resetForm }) => {
+        const result = await createCongressInstantMessage({ ...values, cgwcId: CongressId });
+
+        if ('data' in result) {
+            setModalState({
+                message: 'Message successfully created',
+                status: 'success',
+            });
+            reset();
+            resetForm({ values: INITIAL_VALUES });
+            navigate('Congress Details', data);
+        }
+
+        if ('error' in result) {
+            setModalState({
+                message: 'Oops something went wrong',
+                status: 'error',
+            });
+        }
+    };
+
+    const INITIAL_VALUES: ICongressInstantMessage = {
+        CongressId,
+        title: '',
+        message: '',
+        messageLink: '',
+        status: 'PENDING',
+    } as ICongressInstantMessage;
+
+    return (
+        <ViewWrapper scroll noPadding>
+            <View space="lg" alignItems="flex-start" w="100%" mb={24} className="px-4">
+                <View alignItems="center" w="100%">
+                    <Formik<ICongressInstantMessage>
+                        validateOnChange
+                        onSubmit={onSubmit}
+                        initialValues={INITIAL_VALUES}
+                        validationSchema={CreateCongressInstantMessageSchema}
+                    >
+                        {({ errors, values, handleChange, handleSubmit, touched }) => (
+                            <View w="100%" space={4}>
+                                <FormControl isRequired isInvalid={!!errors?.title && touched.title}>
+                                    <FormControl.Label>Title</FormControl.Label>
+                                    <InputComponent
+                                        placeholder="Title"
+                                        value={values.title}
+                                        onChangeText={handleChange('title')}
+                                    />
+                                    <FormControl.ErrorMessage
+                                        fontSize="2xl"
+                                        mt={3}
+                                        leftIcon={
+                                            <Icon
+                                                size={16}
+                                                name="warning"
+                                                type="antdesign"
+                                                color={THEME_CONFIG.error}
+                                            />
+                                        }
+                                    >
+                                        {errors?.title}
+                                    </FormControl.ErrorMessage>
+                                </FormControl>
+
+                                <FormControl isRequired isInvalid={!!errors?.message && touched.message}>
+                                    <FormControl.Label>Message</FormControl.Label>
+                                    <TextAreaComponent
+                                        value={values.message}
+                                        placeholder="Message"
+                                        onChangeText={handleChange('message')}
+                                    />
+                                    <FormControl.ErrorMessage
+                                        fontSize="2xl"
+                                        mt={3}
+                                        leftIcon={
+                                            <Icon
+                                                size={16}
+                                                name="warning"
+                                                type="antdesign"
+                                                color={THEME_CONFIG.error}
+                                            />
+                                        }
+                                    >
+                                        {errors?.message}
+                                    </FormControl.ErrorMessage>
+                                </FormControl>
+
+                                <FormControl>
+                                    <ButtonComponent
+                                        mt={4}
+                                        isLoading={isLoading}
+                                        onPress={handleSubmit as (event: any) => void}
+                                    >
+                                        Post Instant Message
+                                    </ButtonComponent>
+                                </FormControl>
+                            </View>
+                        )}
+                    </Formik>
+                </View>
+            </View>
+        </ViewWrapper>
+    );
+};
+
+export default React.memo(CreateCongressInstantMessage);
