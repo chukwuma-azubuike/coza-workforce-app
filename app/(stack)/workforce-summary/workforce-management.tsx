56c39dd541a7756ca7c4898ebf80b1e752594375
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-05-22 16:29:35 +0100

    revert-1

--- app/(stack)/workforce-summary/workforce-management.tsx
+++ app/(stack)/workforce-summary/workforce-management.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import WorkforceManagement from '~/views/app/workforce-management/workforce-management';
+// import WorkforceManagement from '~/views/app/workforce-management/workforce-management';
 
 const WorkforcemanagementScreen: React.FC = () => {
-    return <WorkforceManagement />;
+    return <></>;
 };
 
 export default WorkforcemanagementScreen;
