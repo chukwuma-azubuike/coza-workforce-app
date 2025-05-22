867f6aa1555ee7adad6d29dd50a44df5dbcd93f7
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-05-22 16:53:12 +0100

    restore metro load up

--- components/atoms/logo/index.tsx
+++ components/atoms/logo/index.tsx
@@ -7,7 +7,7 @@ const logoBlack = require('~/assets/images/COZA-Logo-black.png');
 const Logo: React.FC = () => {
     const { isLightColorScheme } = useColorScheme();
 
-    return <Image source={isLightColorScheme ? logoBlack : logoWhite} />;
+    return <Image style={{ width: 120, height: 120 }} source={isLightColorScheme ? logoBlack : logoWhite} />;
 };
 
 export default React.memo(Logo);
