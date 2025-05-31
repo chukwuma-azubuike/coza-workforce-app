7d4fc105a6ddce48d521ce146083d7bd3ea26cbb
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-05-31 15:41:01 +0100

    update env config, clockin range for dev & gh clocker styling

--- config/envConfig.ts
+++ config/envConfig.ts
@@ -2,6 +2,7 @@ export const ENV = process.env.EXPO_PUBLIC_ENV;
 
 const APP_ENV = {
     ENV,
+    isProd: ENV === 'production',
     API_KEY: process.env.EXPO_PUBLIC_API_KEY || '',
     API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL || '',
     APP_NAME: process.env.EXPO_PUBLIC_APP_NAME || '',
