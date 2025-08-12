72a9020887d346d9fe4f7873374f19d665654a64
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-08-12 22:48:24 +0100

    animated tab slot

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,8 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { Tabs, TabList, TabTrigger, TabSlot } from 'expo-router/ui';
+import Animated, { useSharedValue, useAnimatedStyle, withTiming, Easing } from 'react-native-reanimated';
 import * as Haptics from 'expo-haptics';
+import { usePathname } from 'expo-router';
 
 import { TabButton } from '~/components/TabButton';
 import { NavTabBackground } from '~/components/NavBackgroundBlur';
@@ -8,12 +10,29 @@ import { NavTabBackground } from '~/components/NavBackgroundBlur';
 import { AppRoutes } from '@config/navigation';
 import useRole from '@hooks/role';
 import { cn } from '~/lib/utils';
-import { View } from 'react-native';
 
 const TabLayout: React.FC = () => {
     const { isWorker, isQC, isCGWCApproved } = useRole();
 
     const tabRoutes = useMemo(() => AppRoutes.filter(route => route.inMenuBar), [AppRoutes]);
+
+    const pathname = usePathname();
+    const progress = useSharedValue(1);
+
+    useEffect(() => {
+        // animate from 0 -> 1 on path change
+        progress.value = 0;
+        progress.value = withTiming(1, {
+            duration: 600,
+            easing: Easing.out(Easing.cubic),
+        });
+    }, [pathname]);
+
+    const style = useAnimatedStyle(() => ({
+        opacity: progress.value,
+        // transform: [{ translateY: (1 - progress.value) * 8 }],
+        // you can also compute pointer events / z-index here if needed
+    }));
 
     return (
         <Tabs
@@ -27,9 +46,10 @@ const TabLayout: React.FC = () => {
             }}
             className="shadow-lg shadow-black/5 flex-1"
         >
-            <View style={{ flex: 1, marginBottom: 90 }}>
+            <Animated.View style={[{ flex: 1, marginBottom: 90 }, style]}>
                 <TabSlot />
-            </View>
+            </Animated.View>
+
             <TabList asChild className={cn('absolute !bg-background bottom-0 left-0 right-0 z-10 overflow-x-auto')}>
                 <NavTabBackground>
                     {tabRoutes.map((route, index) => {
