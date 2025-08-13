f4a24e2041eefcb91d63c8fdc66205f3c49be17d
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-08-13 17:45:46 +0100

    in app updates

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
 module.exports = function (api) {
     api.cache(true);
     return {
-        presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+        presets: [
+            ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+            'nativewind/babel',
+        ],
+        plugins: [
+            [
+                'module-resolver',
+                {
+                    root: ['.'],
+                    alias: {
+                        'react-native-device-info': './react-native-device-info.js',
+                    },
+                },
+            ],
+        ],
     };
 };
