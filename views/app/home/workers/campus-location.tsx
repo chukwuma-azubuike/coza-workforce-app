486ca611a6ec92947e5b0c5b4054e44e9959b1af
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-08-29 17:05:28 +0100

    fix: 🐛 conditionally render location icon on home screen
    implement conditional opacity of clock button text

--- views/app/home/workers/campus-location.tsx
+++ views/app/home/workers/campus-location.tsx
@@ -13,11 +13,11 @@ const CampusLocation = () => {
     return (
         <View className="items-center">
             <View className="flex-row items-center">
-                <Ionicons
+               {data?.campus?.campusName && <Ionicons
                     color={!isError && !isLoading ? THEME_CONFIG.gray : 'transparent'}
                     name="location-sharp"
                     size={15}
-                />
+                />}
                 <Text className="font-semibold text-muted-foreground text-base ml-1">
                     {!isError ? data?.campus?.campusName : ''}
                 </Text>
