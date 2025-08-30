fbdab5682fac448bfd2e88417d106d7121e528dc
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-08-30 21:23:30 +0100

    fix: 🐛 remove awkward pdding at bottom of android home screen

--- components/NavBackgroundBlur.tsx
+++ components/NavBackgroundBlur.tsx
@@ -1,12 +1,14 @@
 import { Ref, forwardRef } from 'react';
 
 import { TabListProps } from 'expo-router/ui';
-import { View } from 'react-native';
+import { Platform, View } from 'react-native';
 import { cn } from '~/lib/utils';
 
 export type NavTabBackgroundProps = TabListProps & {};
 
 const NavTabBackground = forwardRef(({ children, className, ...props }: NavTabBackgroundProps, ref: Ref<View>) => {
+    const isAndroid = Platform.OS == 'android';
+
     return (
         <View
             ref={ref}
@@ -14,6 +16,7 @@ const NavTabBackground = forwardRef(({ children, className, ...props }: NavTabBa
             style={{ backgroundColor: 'transparent' }}
             className={cn(
                 `w-full !bg-transparent flex-row pb-10 !justify-around pt-4 border-t border-border/60`,
+                isAndroid && 'pb-4',
                 className
             )}
         >
