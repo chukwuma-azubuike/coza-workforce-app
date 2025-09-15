74a780a83e37b75bab3cdc1f15dd9e0b87973354
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-09-15 08:10:47 +0100

    feat: 🎸 route setup

---
+++ app/(roast-crm)/_layout.tsx
@@ -0,0 +1,128 @@
+import React, { useEffect, useMemo } from 'react';
+import { Tabs, TabList, TabTrigger, TabSlot } from 'expo-router/ui';
+import Animated, { useSharedValue, useAnimatedStyle, withTiming, Easing } from 'react-native-reanimated';
+import * as Haptics from 'expo-haptics';
+import { usePathname } from 'expo-router';
+
+import { TabButton } from '~/components/TabButton';
+import { NavTabBackground } from '~/components/NavBackgroundBlur';
+
+import useRole, { ROLES } from '@hooks/role';
+import { cn } from '~/lib/utils';
+import { Platform, SafeAreaView } from 'react-native';
+import TopNav from '~/components/TopNav';
+
+const tabRoutes = [
+    {
+        name: 'My Guests',
+        href: '/my-guests',
+        options: { title: 'My Guests' },
+        users: ['all'],
+        inMenuBar: true,
+        inMore: false,
+        icon: { name: 'user-friends', type: 'font-awesome-5' },
+    },
+    {
+        name: 'Zone Dashboard',
+        options: { title: 'Zone Dashboard' },
+        users: [ROLES.zonalCoordinator],
+        inMenuBar: true,
+        inMore: false,
+        icon: { name: 'map-marked-alt', type: 'font-awesome-5' },
+        href: '/zone-dashboard',
+    },
+    {
+        name: 'Global Dashboard',
+        options: { title: 'Global Dashboard' },
+        users: [ROLES.superAdmin, ROLES.globalAdmin, ROLES.campusPastor, ROLES.globalPastor],
+        inMenuBar: true,
+        inMore: false,
+        icon: { name: 'globe', type: 'font-awesome' },
+        href: '/global-dashboard',
+    },
+    {
+        name: 'Notifications',
+        options: { title: 'Notifications' },
+        users: ['all'],
+        inMenuBar: true,
+        inMore: false,
+        icon: { name: 'notifications', type: 'ionicon' },
+        href: '/notifications',
+    },
+    {
+        name: 'Settings',
+        options: { title: 'Settings' },
+        users: [ROLES.superAdmin, ROLES.globalAdmin],
+        inMenuBar: true,
+        inMore: false,
+        icon: { name: 'settings-sharp', type: 'ionicon' },
+        href: '/settings',
+    },
+];
+
+const TabLayout: React.FC = () => {
+    const { role } = useRole();
+
+    const pathname = usePathname();
+    const progress = useSharedValue(1);
+    const isAndroid = Platform.OS === 'android';
+
+    const filteredRoutes = useMemo(
+        () => tabRoutes.filter(route => route.users.includes('all') || route.users.includes(role as string)),
+        [tabRoutes, role]
+    );
+
+    useEffect(() => {
+        // animate from 0 -> 1 on path change
+        progress.value = 0;
+        progress.value = withTiming(1, {
+            duration: 600,
+            easing: Easing.out(Easing.cubic),
+        });
+    }, [pathname]);
+
+    const style = useAnimatedStyle(() => ({
+        opacity: progress.value,
+    }));
+
+    return (
+        <Tabs
+            options={{
+                backBehavior: 'order',
+                screenListeners: {
+                    tabPress: () => {
+                        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+                    },
+                },
+            }}
+            className="shadow-lg shadow-black/5 flex-1"
+        >
+            <SafeAreaView className="flex-1">
+                <Animated.View style={[{ flex: 1, marginBottom: isAndroid ? 70 : 90 }, style]}>
+                    <TopNav />
+                    <TabSlot />
+                </Animated.View>
+            </SafeAreaView>
+
+            <TabList asChild className={cn('absolute !bg-background bottom-0 left-0 right-0 z-10 overflow-x-auto')}>
+                <NavTabBackground>
+                    {filteredRoutes.map((route, index) => (
+                        <TabTrigger
+                            asChild
+                            reset="always"
+                            name={route.name}
+                            key={`route-${index}`}
+                            href={route.href as any}
+                        >
+                            <TabButton iconName={route.icon.name} iconType={route.icon.type} className="w-28">
+                                {route.name}
+                            </TabButton>
+                        </TabTrigger>
+                    ))}
+                </NavTabBackground>
+            </TabList>
+        </Tabs>
+    );
+};
+
+export default TabLayout;
--- components/Routing.tsx
+++ components/Routing.tsx
@@ -9,16 +9,26 @@ import { userSelectors } from '~/store/actions/users';
 import { NotificationsProvider } from './NotificationsProvider';
 import inAppUpdates from '~/utils/in-app-updates';
 import { useAuth } from '~/hooks/auth';
+import { appSelectors } from '~/store/actions/app';
 
 export { ErrorBoundary } from 'expo-router';
 
 const Routing: React.FC = () => {
-    const user = useAppSelector(userSelectors.selectCurrentUser);
     const { logOut } = useAuth();
+    const user = useAppSelector(userSelectors.selectCurrentUser);
+    const mode = useAppSelector(appSelectors.selectMode);
+
+    const routeToMode = () => {
+        if (mode === 'crm') {
+            router.replace('/(roast-crm)');
+        } else {
+            router.replace('/(tabs)');
+        }
+    };
 
     React.useEffect(() => {
         if (user?.userId) {
-            router.replace('/(tabs)');
+            routeToMode();
         } else {
             logOut();
         }
@@ -28,7 +38,7 @@ const Routing: React.FC = () => {
         };
 
         update();
-    }, [user?.userId]);
+    }, [user?.userId, mode]);
 
     return (
         <NotificationsProvider user={user || ({} as any)}>
