2c3608e8239d87dfa893ea906cefb94ab27fa871
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-09-15 14:41:33 +0100

    feat: 🎸 leaderboards and my guest dashboard route

--- components/atoms/button/index.tsx
+++ components/atoms/button/index.tsx
@@ -181,22 +181,27 @@ export const NavigationBackButton: React.FC = React.memo(props => {
     );
 });
 
-export const FloatButton: React.FC<ButtonProps & { iconName: string; iconType: string; className: string }> =
-    React.memo(({ iconName, iconType, className, ...props }) => {
-        return (
-            <Button
-                style={{
-                    right: 25,
-                    bottom: 128,
-                    zIndex: 10,
-                    position: 'absolute',
-                }}
-                className={cn('!w-[4.4rem] !h-[4.4rem] px-0', className)}
-                {...props}
-            >
-                <Icon name={iconName} type={iconType} size={28} color="white" />
-            </Button>
-        );
-    });
+export const FloatButton: React.FC<
+    ButtonProps & { iconName?: string; iconType?: string; className?: string; iconClassname?: string }
+> = React.memo(({ iconName, iconClassname, iconType, className, children, ...props }) => {
+    return (
+        <Button
+            style={{
+                right: 10,
+                bottom: 15,
+                zIndex: 10,
+                position: 'absolute',
+            }}
+            className={cn('!h-[3rem] gap-0.5 flex-row items-center !rounded-full', className)}
+            {...props}
+        >
+            {iconName && iconType && (
+                <Icon name={iconName} type={iconType} size={14} color="white" className={iconClassname} />
+            )}
+
+            {typeof children === 'string' && <Text className="text-white font-bold">{children}</Text>}
+        </Button>
+    );
+});
 
 export default React.memo(ButtonComponent);
