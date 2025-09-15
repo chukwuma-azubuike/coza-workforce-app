5677734ee0decf00efc7ce2883db65deacc0e48d
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-09-15 08:10:47 +0100

    feat: 🎸 route setup

---
+++ app/(roast-crm)/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Home from '~/views/app/home';
+
+const RoastCRMScreen: React.FC = () => {
+    return <Home />;
+};
+
+export default RoastCRMScreen;
---
+++ app/(roast-crm)/notifications/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Attendance from '~/views/app/attendance';
+
+const MyGuestsScreen: React.FC = () => {
+    return <Attendance />;
+};
+
+export default MyGuestsScreen;
