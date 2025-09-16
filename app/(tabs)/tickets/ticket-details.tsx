28c3f8b6514c6808adb63f8b0810c5b5639081fd
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-09-16 04:03:17 +0100

    refactor: 💡 performance optimization

--- app/(tabs)/tickets/ticket-details.tsx
+++ app/(tabs)/tickets/ticket-details.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import TicketDetails from '~/views/app/tickets/ticket-details';
+import React, { Suspense } from 'react';
+import Loading from '~/components/atoms/loading';
+const TicketDetails = React.lazy(() => import('~/views/app/tickets/ticket-details'));
 
 const TicketsDetailsScreen: React.FC = () => {
-    return <TicketDetails />;
+    return (
+        <Suspense fallback={<Loading cover />}>
+            <TicketDetails />
+        </Suspense>
+    );
 };
 
 export default TicketsDetailsScreen;
