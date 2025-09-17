6770e337a3a04b8243af70a3e7de1d854f9e5219
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-09-17 09:20:46 +0100

    fix: 🐛 resolve ui jump on major route change
    add SafeareaProvider and SafeArea View at the highest routing level

--- components/Routing.tsx
+++ components/Routing.tsx
@@ -8,6 +8,7 @@ import { userSelectors } from '~/store/actions/users';
 import { NotificationsProvider } from './NotificationsProvider';
 import inAppUpdates from '~/utils/in-app-updates';
 import { appSelectors } from '~/store/actions/app';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export { ErrorBoundary } from 'expo-router';
 
@@ -39,17 +40,24 @@ const Routing: React.FC = () => {
 
     return (
         <NotificationsProvider user={user || ({} as any)}>
-            <View className="flex-1">
-                <NotificationModal />
-                <Stack>
-                    <Stack.Screen name="index" options={{ headerShown: false }} />
-                    <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-                    <Stack.Screen name="(stack)" options={{ headerShown: false }} />
-                    <Stack.Screen name="(tabs)" options={{ headerShown: false, gestureEnabled: false }} />
-                    <Stack.Screen name="roast-crm/(tabs)" options={{ headerShown: false, gestureEnabled: false }} />
-                    <Stack.Screen name="roast-crm/(stack)" options={{ headerShown: false }} />
-                </Stack>
-            </View>
+            <SafeAreaProvider>
+                <SafeAreaView className="flex-1 bg-background">
+                    <View className="flex-1">
+                        <NotificationModal />
+                        <Stack>
+                            <Stack.Screen name="index" options={{ headerShown: false }} />
+                            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+                            <Stack.Screen name="(stack)" options={{ headerShown: false }} />
+                            <Stack.Screen name="(tabs)" options={{ headerShown: false, gestureEnabled: false }} />
+                            <Stack.Screen
+                                name="roast-crm/(tabs)"
+                                options={{ headerShown: false, gestureEnabled: false }}
+                            />
+                            <Stack.Screen name="roast-crm/(stack)" options={{ headerShown: false }} />
+                        </Stack>
+                    </View>
+                </SafeAreaView>
+            </SafeAreaProvider>
         </NotificationsProvider>
     );
 };
