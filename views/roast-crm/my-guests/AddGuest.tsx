6122f50b8a6f8c6e8eb6029e4de178111436fe35
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-09-17 12:54:58 +0100

    fix: 🐛 add guest modal background color

--- views/roast-crm/my-guests/AddGuest.tsx
+++ views/roast-crm/my-guests/AddGuest.tsx
@@ -1,6 +1,8 @@
+import { X } from 'lucide-react-native';
 import React, { Suspense } from 'react';
-import { Modal, View, Button } from 'react-native';
+import { Modal, View } from 'react-native';
 import Loading from '~/components/atoms/loading';
+import { Button } from '~/components/ui/button';
 import { Text } from '~/components/ui/text';
 
 const GuestCaptureForm = React.lazy(() => import('./guests/GuestCaptureForm'));
@@ -21,17 +23,19 @@ const AddGuestModal: React.FC<AddGuestModalProps> = ({ modalVisible, setModalVis
         >
             <View
                 style={{ justifyContent: 'flex-end' }}
-                className="bg-secondary dark:bg-secondary/40 rounded-t-lg pb-12 flex-1"
+                className="bg-secondary dark:bg-background rounded-t-lg pb-8 flex-1"
             >
-                <View className="bg-secondary/40 rounded-t-2xl p-6">
+                <View className="bg-secondary/40 rounded-t-2xl p-6 relative">
                     <Text className="font-bold text-lg text-center">Add Guest</Text>
+                    <Button variant="ghost" className="absolute top-5 right-4 !h-8 w-8" onPress={setModalVisible}>
+                        <X className="w-4 h-4 text-muted-foreground" />
+                    </Button>
                 </View>
                 <View className="flex-1 px-2">
                     <Suspense fallback={<Loading cover />}>
                         <GuestCaptureForm />
                     </Suspense>
                 </View>
-                <Button title="Cancel" onPress={setModalVisible} />
             </View>
         </Modal>
     );
