806e7dcae8152c4070e96eb1cee9ce2eaabdee37
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-09-20 18:44:08 +0100

    feat: 🎸 zone dashboard core ui implementation

---
+++ app/roast-crm/(tabs)/zone-dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ZoneDashboard } from '~/views/roast-crm';
+
+const ZoneDashboardScreen: React.FC = () => {
+    return <ZoneDashboard />;
+};
+
+export default ZoneDashboardScreen;
