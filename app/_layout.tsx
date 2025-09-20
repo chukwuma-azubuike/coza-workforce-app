b5d566d8c260498ddf20eefba3f8e8302e81289b
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-09-20 18:44:08 +0100

    feat: 🎸 zone dashboard core ui implementation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,6 +11,7 @@ import { PortalHost } from '@rn-primitives/portal';
 import ConnectionStatusBar from '~/components/atoms/status-bar';
 import { setAndroidNavigationBar } from '~/lib/android-navigation-bar';
 import * as Notifications from 'expo-notifications';
+import { useFonts } from 'expo-font';
 
 import '~/global.css';
 import Routing from '~/components/Routing';
@@ -46,8 +47,12 @@ export default function RootLayout() {
     const { colorScheme, isDarkColorScheme } = useColorScheme();
     const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
 
+    const [loaded, error] = useFonts({
+        Angelos: require('../assets/fonts/Angelos.ttf'),
+    });
+
     useIsomorphicLayoutEffect(() => {
-        if (hasMounted.current) {
+        if (hasMounted.current && !loaded) {
             return;
         }
 
@@ -62,7 +67,7 @@ export default function RootLayout() {
 
     useNotificationObserver();
 
-    if (!isColorSchemeLoaded) {
+    if (!isColorSchemeLoaded || (!loaded && !error)) {
         return null;
     }
 
