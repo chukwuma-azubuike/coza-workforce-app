1d72426f9fd8aefea90521937b58e200c28b5158
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-09-23 00:35:23 +0100

    feat: 🎸 global dashboard with dummy data

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -9,7 +9,7 @@ const TabsList = React.forwardRef<TabsPrimitive.ListRef, TabsPrimitive.ListProps
     <TabsPrimitive.List
         ref={ref}
         className={cn(
-            'web:inline-flex h-10 native:h-12 items-center justify-center rounded-md bg-muted p-1 native:px-1.5',
+            'web:inline-flex h-10 native:h-12 items-center justify-center rounded-xl bg-muted p-1 !native:px-1.5',
             className
         )}
         {...props}
@@ -30,7 +30,7 @@ const TabsTrigger = React.forwardRef<TabsPrimitive.TriggerRef, TabsPrimitive.Tri
                 <TabsPrimitive.Trigger
                     ref={ref}
                     className={cn(
-                        'inline-flex items-center justify-center shadow-none web:whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium web:ring-offset-background web:transition-all web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2',
+                        'inline-flex items-center justify-center shadow-none web:whitespace-nowrap rounded-xl px-3 py-1.5 text-sm font-medium web:ring-offset-background web:transition-all web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2',
                         props.disabled && 'web:pointer-events-none opacity-50',
                         props.value === value && 'bg-background shadow-lg shadow-foreground/10',
                         className
