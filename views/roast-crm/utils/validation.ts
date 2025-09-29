ceba0e239456a35c81353c20a557f12d88c8016b
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-09-29 14:42:05 +0100

    fix: 🐛 make address optional in guest form schema validation

--- views/roast-crm/utils/validation.ts
+++ views/roast-crm/utils/validation.ts
@@ -5,7 +5,7 @@ import { countryCodeIso } from '~/utils/countryCodeIso';
 export const GuestFormValidationSchema = Yup.object().shape({
     firstName: Yup.string().required("Guest's first name is required."),
     lastName: Yup.string().optional(),
-    address: Yup.string().required('Your home address is required.'),
+    address: Yup.string().optional(),
     phoneNumber: Yup.string()
         .phone(countryCodeIso as any, 'Please enter a valid phone number')
         .required('A phone number is required'),
