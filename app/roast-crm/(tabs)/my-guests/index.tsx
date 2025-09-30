3aca6f7c1a62e3f387192ef2f850cdfb051c8cff
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-09-30 15:32:53 +0100

    perf: ⚡️ code splitting via lazy loading on tab routes

--- app/roast-crm/(tabs)/my-guests/index.tsx
+++ app/roast-crm/(tabs)/my-guests/index.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import { MyGuestsDashboard } from '~/views/roast-crm/my-guests';
+import React, { Suspense } from 'react';
+import Loading from '~/components/atoms/loading';
+
+const MyGuestsDashboard = React.lazy(() => import('~/views/roast-crm/my-guests/MyGuestsDashboard'));
 
 const MyGuestsScreen: React.FC = () => {
-    return <MyGuestsDashboard />;
+    return (
+        <Suspense fallback={<Loading cover />}>
+            <MyGuestsDashboard />
+        </Suspense>
+    );
 };
 
 export default MyGuestsScreen;
