f610b70c4cee730572284307897c4b4fb1a1858b
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-10-17 17:21:51 +0100

    fix: 🐛 conditinally fetch and instal eas updates

--- hooks/expo-update/index.ts
+++ hooks/expo-update/index.ts
@@ -2,19 +2,19 @@ import { useEffect } from 'react';
 import * as Updates from 'expo-updates';
 
 const useExpoUpdate = () => {
-    const { isUpdateAvailable, isUpdatePending } = Updates.useUpdates();
+    const { isUpdateAvailable } = Updates.useUpdates();
 
     useEffect(() => {
+        if (!isUpdateAvailable) return;
+
         // Update is available; download and apply it now
         Updates.fetchUpdateAsync()
             .then(() => {
-                if (isUpdatePending) {
-                    Updates.reloadAsync().catch();
-                }
+                Updates.reloadAsync().catch();
             })
             .catch()
             .finally();
-    }, [isUpdateAvailable, isUpdatePending]);
+    }, [isUpdateAvailable]);
 };
 
 export default useExpoUpdate;
