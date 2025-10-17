04d0a8071757a2a663d0a72af1ee42b8eb0b58dd
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-10-17 17:18:40 +0100

    fix: 🐛 add condition to reloadAsync EASUpdate

--- hooks/expo-update/index.ts
+++ hooks/expo-update/index.ts
@@ -2,17 +2,19 @@ import { useEffect } from 'react';
 import * as Updates from 'expo-updates';
 
 const useExpoUpdate = () => {
-    const { isUpdateAvailable } = Updates.useUpdates();
+    const { isUpdateAvailable, isUpdatePending } = Updates.useUpdates();
 
     useEffect(() => {
         // Update is available; download and apply it now
         Updates.fetchUpdateAsync()
             .then(() => {
-                Updates.reloadAsync().catch();
+                if (isUpdatePending) {
+                    Updates.reloadAsync().catch();
+                }
             })
             .catch()
             .finally();
-    }, [isUpdateAvailable]);
+    }, [isUpdateAvailable, isUpdatePending]);
 };
 
 export default useExpoUpdate;
