c1830028c7975cdf8a57c62ee370e6efdcd1e247
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-10-23 11:41:24 +0100

    fix: 🐛 clean up zone item list ui

--- views/roast-crm/settings/components/ZoneListItem.tsx
+++ views/roast-crm/settings/components/ZoneListItem.tsx
@@ -1,10 +1,11 @@
 import dayjs from 'dayjs';
-import { Clock, MapPin } from 'lucide-react-native';
+import { Clock, Home, MapPin } from 'lucide-react-native';
 import React, { Suspense, useCallback, useState } from 'react';
 import { TouchableOpacity, View } from 'react-native';
 import { Text } from '~/components/ui/text';
 import { Zone } from '~/store/types';
 import FlatListComponent from '~/components/composite/flat-list';
+import { Badge } from '~/components/ui/badge';
 const AddZoneModal = React.lazy(() => import('./AddModalZone'));
 const EditZoneModal = React.lazy(() => import('./EditModalZone'));
 
@@ -19,8 +20,8 @@ const ZoneListItem: React.FC<Zone & { openModal: (zone: Zone) => void }> = ({ op
         <View className="py-4 w-full border-t border-t-border">
             <TouchableOpacity activeOpacity={0.6} onPress={handleViewZone}>
                 <View className="flex-row items-start justify-between mb-2">
-                    <View className="gap-1">
-                        <Text className="font-bold text-xl">{name}</Text>
+                    <View className="gap-1 flex-row justify-between items-center">
+                        <Text className="font-bold text-3xl">{name}</Text>
                         <View className="flex-row gap-1 items-center">
                             <MapPin size={14} />
                             <Text className="text-xs text-foreground">
@@ -33,21 +34,21 @@ const ZoneListItem: React.FC<Zone & { openModal: (zone: Zone) => void }> = ({ op
                 <View className="gap-3">
                     <View className="flex-row flex-wrap gap-2">
                         {departments?.map(dept => (
-                            <Text key={dept.id} className="font-bold text-muted-foreground">
-                                • {dept.name}
-                            </Text>
+                            <Badge variant="outline" key={dept.id}>
+                                <Text className="text-base">{dept.name}</Text>
+                            </Badge>
                         ))}
                     </View>
 
                     {address && (
-                        <View className="text-xs flex-row bg-blue-50 dark:bg-blue-400/20 border border-blue-200 dark:border-blue-500/20 rounded p-2">
-                            <Text className="font-bold">Address: </Text>
+                        <View className="text-xs flex-row border border-blue-200 dark:border-blue-500/20 rounded-xl p-2 items-center gap-2">
+                            <Home size={18} />
                             <Text className="line-clamp-none">{address}</Text>
                         </View>
                     )}
 
                     {descriptions && (
-                        <View className="text-xs bg-blue-50 dark:bg-blue-400/20 border border-blue-200 dark:border-blue-500/20 rounded p-2">
+                        <View className="text-xs border border-blue-200 dark:border-blue-500/20 rounded-xl p-2">
                             <Text className="font-bold">Description: </Text>
                             <Text className="line-clamp-none">{descriptions}</Text>
                         </View>
@@ -56,7 +57,7 @@ const ZoneListItem: React.FC<Zone & { openModal: (zone: Zone) => void }> = ({ op
                     <View className="flex-row items-center justify-between text-xs">
                         <View className="flex-row items-center gap-2 text-foreground">
                             <Clock className="w-3 h-3" />
-                            <Text>Created on {dayjs(createdAt).format('DD/MM/YYYY')}</Text>
+                            <Text className="text-base">Created on {dayjs(createdAt).format('DD/MM/YYYY')}</Text>
                         </View>
                     </View>
                 </View>
