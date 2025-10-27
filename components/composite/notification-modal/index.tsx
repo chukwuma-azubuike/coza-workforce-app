f7cb1cad48c2a26c73f60a07c2fc9c1aaf6aed41
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-10-27 21:57:19 +0100

    fix: 🐛 safe area view for android

--- components/composite/notification-modal/index.tsx
+++ components/composite/notification-modal/index.tsx
@@ -36,7 +36,7 @@ const NotificationModal: React.FC = () => {
     return (
         <Dialog open={open || !!render || !!message} onOpenChange={hideModal}>
             <DialogContent className="h-72 !w-20" showClose={false}>
-                <View className="justify-center">
+                <View className="justify-center mx-auto">
                     <View className="w-full gap-2 rounded-2xl bg-transparent">
                         {render ? (
                             <ModalAlertComponent
