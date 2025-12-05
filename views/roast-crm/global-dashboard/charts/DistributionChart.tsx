366cd21fa0b925b8bc8da562496314ecb937eba2
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2025-12-05 13:01:28 +0100

    feat: 🎸 global dashboard api integration

--- views/roast-crm/global-dashboard/charts/DistributionChart.tsx
+++ views/roast-crm/global-dashboard/charts/DistributionChart.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Pie, PolarChart } from 'victory-native';
 import { Card, CardHeader, CardTitle, CardContent } from '~/components/ui/card';
-import { DATA } from './data';
 import { useFont } from '@shopify/react-native-skia';
 import NexaExtraLight from '~/assets/fonts/Nexa-ExtraLight.ttf';
+import Empty from '~/components/atoms/empty';
 
 interface DistributionChartProps {
     data: Array<{
@@ -22,24 +22,28 @@ export function DistributionChart({ data }: DistributionChartProps) {
                 <CardTitle>Guest Distribution by Stage</CardTitle>
             </CardHeader>
             <CardContent style={{ height: 300 }}>
-                <PolarChart data={data ?? DATA} labelKey="name" valueKey="value" colorKey="color">
-                    <Pie.Chart>
-                        {() => (
-                            <>
-                                <Pie.Slice>
-                                    <Pie.Label font={font} color="#FFF" />
-                                </Pie.Slice>
+                {data.length > 0 ? (
+                    <PolarChart data={data} labelKey="name" valueKey="value" colorKey="color">
+                        <Pie.Chart>
+                            {() => (
+                                <>
+                                    <Pie.Slice>
+                                        <Pie.Label font={font} color="#FFF" />
+                                    </Pie.Slice>
 
-                                <Pie.SliceAngularInset
-                                    angularInset={{
-                                        angularStrokeWidth: 0.7,
-                                        angularStrokeColor: '#FFF',
-                                    }}
-                                />
-                            </>
-                        )}
-                    </Pie.Chart>
-                </PolarChart>
+                                    <Pie.SliceAngularInset
+                                        angularInset={{
+                                            angularStrokeWidth: 0.7,
+                                            angularStrokeColor: '#FFF',
+                                        }}
+                                    />
+                                </>
+                            )}
+                        </Pie.Chart>
+                    </PolarChart>
+                ) : (
+                    <Empty width={200} />
+                )}
             </CardContent>
         </Card>
     );
--- views/roast-crm/global-dashboard/performers/TopPerformers.tsx
+++ views/roast-crm/global-dashboard/performers/TopPerformers.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
 import { Card, CardHeader, CardTitle, CardContent } from '~/components/ui/card';
 import { TopPerformerCard } from './TopPerformerCard';
-import { TrendDirection } from '~/store/types';
+import { TopPerformer } from '~/store/types';
 import { View } from 'react-native';
 
 interface TopPerformersProps {
-    performers: Array<{
-        name: string;
-        zone: string;
-        conversions: number;
-        trend: TrendDirection;
-    }>;
+    performers: Array<TopPerformer>;
 }
 
-export function TopPerformers({ performers }: TopPerformersProps) {
+export function TopPerformingZones({ performers }: TopPerformersProps) {
+    return (
+        <Card className="flex-1">
+            <CardHeader>
+                <CardTitle>Top Performing Zones</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <View className="gap-3">
+                    {performers.map((performer, index) => (
+                        <TopPerformerCard key={performer.name} {...(performer as any)} rank={index + 1} />
+                    ))}
+                </View>
+            </CardContent>
+        </Card>
+    );
+}
+
+export function TopPerformingWorkers({ performers }: TopPerformersProps) {
     return (
         <Card className="flex-1">
             <CardHeader>
@@ -22,7 +33,7 @@ export function TopPerformers({ performers }: TopPerformersProps) {
             <CardContent>
                 <View className="gap-3">
                     {performers.map((performer, index) => (
-                        <TopPerformerCard key={performer.name} {...performer} rank={index + 1} />
+                        <TopPerformerCard key={performer.name} {...(performer as any)} rank={index + 1} />
                     ))}
                 </View>
             </CardContent>
