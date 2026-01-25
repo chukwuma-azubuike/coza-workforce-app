47953b51603fa92cd77240e652663f9ba12ccf86
Toluwalope Adeoye <toluade1512@gmail.com>
2026-01-25 20:35:33 +0100

    fix: Correct month payload type to 0-indexed and update Expo and other iOS dependencies.

--- store/services/account.ts
+++ store/services/account.ts
@@ -123,7 +123,7 @@ export type IGetUserByIdResponse = IDefaultResponse<IUser>;
 export interface IGetUserStatusHistoryPayload {
     userId: string;
     year: number;
-    month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+    month: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
     monthsBack: number;
 }
 
