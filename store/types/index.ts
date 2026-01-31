5a5e2912ddb3a759279c19a0d912132015890183
Toluwalope Adeoye <toluade1512@gmail.com>
2026-01-31 18:57:56 +0100

    feat: 🎸 update user status interfaces to include attendance details and refine month type

--- store/types/index.ts
+++ store/types/index.ts
@@ -8,6 +8,7 @@ export interface ILog {
     dateUpdated?: string;
     updatedAt?: string;
 }
+export type Month = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
 export enum ERROR {
     NETWORK_CONNECTION_ERROR = 'TypeError: Network request failed',
