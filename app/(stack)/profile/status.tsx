08d0acbe04c23d1b60c7abed91b48813ea45508f
Toluwalope Adeoye <toluade1512@gmail.com>
2026-01-31 18:47:57 +0100

    feat: 🎸 add Status Report screen and integrate into profile layout

---
+++ app/(stack)/profile/status.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import StatusReport from '~/views/app/profile/status-report';
+
+const StatusReportScreen: React.FC = () => {
+    return <StatusReport />;
+};
+
+export default StatusReportScreen;
