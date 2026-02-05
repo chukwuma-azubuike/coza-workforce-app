db3aae0f15b0d50c072897494e412470d7ffe635
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2026-02-06 00:09:51 +0100

    fix: 🐛 trend chart data map

--- views/roast-crm/global-dashboard/charts/TrendChart.tsx
+++ views/roast-crm/global-dashboard/charts/TrendChart.tsx
@@ -1,17 +1,20 @@
 import React, { useMemo } from 'react';
-import { View, Text } from 'react-native';
 import { CartesianChart, Line, useChartPressState } from 'victory-native';
 import { Circle, useFont } from '@shopify/react-native-skia';
 import { Card, CardHeader, CardTitle, CardContent } from '~/components/ui/card';
 import NexaHeavy from '~/assets/fonts/Nexa-Heavy.ttf';
 import Legend from './Legend';
+import dayjs from 'dayjs';
 
 interface TrendChartProps {
     date: string;
     data: Array<{
         month: string;
         newGuests: number;
-        converted: number;
+        invited: number;
+        attended: number;
+        discipled: number;
+        joined: number;
     }>;
 }
 
@@ -20,11 +23,14 @@ export function TrendChart({ data, date }: TrendChartProps) {
     const { state, isActive } = useChartPressState({ x: 0, y: { value: 0, x: 0, y: 0 } });
 
     const transformedData = useMemo(() => {
-        return data?.map(item => ({
+        return data?.map((item, index) => ({
+            // x: dayjs().add(index, 'hour').unix(),
             value: item.newGuests,
-            x: item.converted,
             y: item.newGuests,
-            // z: item.converted + item.newGuests,
+            invited: item.invited,
+            attended: item.attended,
+            discipled: item.discipled,
+            joined: item.joined,
         }));
     }, [data]);
 
@@ -41,19 +47,19 @@ export function TrendChart({ data, date }: TrendChartProps) {
             <CardContent className="h-72">
                 <Legend />
                 <CartesianChart
-                    xKey="y"
+                    // xKey="x"
                     data={transformedData}
-                    yKeys={['value', 'x', 'y']}
+                    yKeys={['value', 'invited', 'attended', 'discipled', 'joined']}
                     padding={{ left: 10, right: 10, bottom: -40, top: 10 }}
                     domainPadding={{ left: 20, right: 20, top: 30, bottom: 30 }}
                     chartPressState={state}
                     axisOptions={{
                         font: font,
-                        tickCount: 5,
+                        tickCount: data.length,
                         labelColor: '#6B7280',
                         lineColor: 'hsla(0, 0%, 0%, 0.25)',
-                        formatXLabel: () => '',
-                        formatYLabel: value => Math.round(value).toString(),
+                        formatXLabel: value => dayjs(value).format('MMM YYYY'), // or 'MMM'
+                        formatYLabel: value => value.toString(),
                         labelOffset: { x: 5, y: 8 },
                     }}
                 >
@@ -61,46 +67,60 @@ export function TrendChart({ data, date }: TrendChartProps) {
                         <>
                             {/* Curved Lines with spring animation */}
                             <Line
-                                points={points.value}
+                                points={points.attended}
                                 color={'#3B82F6'}
                                 strokeWidth={2}
                                 curveType="catmullRom"
                                 animate={{ type: 'spring', damping: 15, stiffness: 100 }}
                             />
                             <Line
-                                points={points.x}
+                                points={points.joined}
                                 color="#10B981"
                                 strokeWidth={2}
                                 curveType="catmullRom"
                                 animate={{ type: 'spring', damping: 15, stiffness: 100 }}
                             />
-                            {/* <Line
-                                points={points.y}
+                            <Line
+                                points={points.discipled}
                                 color="#8B5CF6"
                                 strokeWidth={2}
                                 curveType="catmullRom"
                                 animate={{ type: 'spring', damping: 15, stiffness: 100 }}
-                            /> */}
+                            />
+                            <Line
+                                points={points.invited}
+                                color="#bcbcbc"
+                                strokeWidth={2}
+                                curveType="catmullRom"
+                                animate={{ type: 'spring', damping: 15, stiffness: 100 }}
+                            />
 
                             {/* Nodes with white border for better visibility */}
-                            {points.value.map((point, index) => (
+                            {points.attended.map((point, index) => (
                                 <React.Fragment key={`value-${index}`}>
                                     <Circle cx={point.x} cy={point.y as any} r={5} color="#FFFFFF" />
                                     <Circle cx={point.x} cy={point.y as any} r={3} color="#3B82F6" />
                                 </React.Fragment>
                             ))}
 
-                            {points.x.map((point, index) => (
+                            {points.joined.map((point, index) => (
                                 <React.Fragment key={`x-${index}`}>
                                     <Circle cx={point.x} cy={point.y as any} r={5} color="#FFFFFF" />
                                     <Circle cx={point.x} cy={point.y as any} r={3} color="#10B981" />
                                 </React.Fragment>
                             ))}
 
-                            {points.y.map((point, index) => (
+                            {points.discipled.map((point, index) => (
                                 <React.Fragment key={`y-${index}`}>
                                     <Circle cx={point.x} cy={point.y as any} r={5} color="#FFFFFF" />
                                     <Circle cx={point.x} cy={point.y as any} r={3} color="#8B5CF6" />
+                                </React.Fragment>
+                            ))}
+
+                            {points.invited.map((point, index) => (
+                                <React.Fragment key={`y-${index}`}>
+                                    <Circle cx={point.x} cy={point.y as any} r={5} color="#FFFFFF" />
+                                    <Circle cx={point.x} cy={point.y as any} r={3} color="#bcbcbc" />
                                 </React.Fragment>
                             ))}
 
