19c912952b9e9ab32ef28fb4af69e42c469f399f
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2026-02-07 18:28:33 +0100

    fix: 🐛 include campus in add guest form & edit profile

--- views/roast-crm/hooks/use-zone-index.tsx
+++ views/roast-crm/hooks/use-zone-index.tsx
@@ -2,7 +2,7 @@ import { useMemo } from 'react';
 import { useGetZonesQuery } from '~/store/services/roast-crm';
 
 const useZoneIndex = () => {
-    const { data: zones } = useGetZonesQuery();
+    const { data: zones } = useGetZonesQuery({});
 
     const assimilationStagesIndex = useMemo(
         () => (zones ? Object.fromEntries(zones?.map(zones => [zones._id, zones.name])) : {}),
