6030d7768f72b7a3612212eedf17b795967bb659
chukwuma-azubuike <chukwumaazubuike@gmail.com>
2026-04-25 13:39:15 +0100

    fix: 🐛 roast safearea ui polish on android & bump build number

--- app/roast-crm/_layout.tsx
+++ app/roast-crm/_layout.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Stack } from 'expo-router';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Platform } from 'react-native';
 import { NavButton } from '~/components/NavButton';
 import CallPromptManager from '~/views/roast-crm/components/CallPromptManager';
 
 const GuestsScreensLayout: React.FC = () => {
     return (
-        <SafeAreaView className="flex-1">
+        <SafeAreaView className="flex-1" edges={['right', 'left', Platform.OS === 'android' ? 'top' : 'bottom']}>
             <Stack screenOptions={{ headerLeft: () => <NavButton />, headerShown: false }}>
                 <Stack.Screen name="index" />
                 <Stack.Screen name="(stack)" />
